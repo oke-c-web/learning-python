bb9b4675c8d524b10b4dadf4899cfa0a126bbdee
oke-c-web <aguval007@gmail.com>
2026-07-10 11:50:02 +0100

    update

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
-print("hello word")
+print("hello")
+
+print("enter your user name")
+
+name = input()
+
+print(f"hi {name} you are welcome to our page!")
+
+print("how many days our you caculating")
+
+def Days(day):
+    HOU = 24
+    MUN = (HOU*60)
+    SEC = (MUN*60)
+
+    print(f"we have {day*HOU} second in {day}")
+    
+Days(int(input()))
